4c0ab0120217c27e63e7eb93bbc3caa6b69bc002
lzenner <lzenner@live.com>
2020-06-27 11:38:46 -1000

    Post-Korlasz exit dialogues are not handled exclusively by the NPC's scripts and dialogues.
    No longer attempting to split gold or swap equipment or track if exit dialogues are finished.  #L_SarvQuests will never be set to 100.  well...never is a strong word...heh

--- dialogues/pk_ajantj.d
+++ dialogues/pk_ajantj.d
@@ -2,22 +2,28 @@
 // Dialogues for Ajantis to allow for him to leave after Korlasz Quest //
 // Compiled via main_common.tpa                                        //
 /////////////////////////////////////////////////////////////////////////
+
+// ToDo: Modify kickout dialogue to have him not stay in the area
+// ToDo: Give him a split of the gold but destroy most of it immediately
+// 		in case he's in SoD or BG2 because he wouldn't carry much with him
+
 APPEND_EARLY AJANTJ
-	IF WEIGHT #0 ~Global("#L_SarvQuests","GLOBAL",99) Global("#L_StayingAfterKorlasz","LOCALS",1)~ THEN BEGIN KORLASZ_EXIT_STAY
-		SAY @2283 /* ~As you wish.  I owe you that at least.~ */
-		IF ~~ THEN EXIT
-	END
-
-	IF WEIGHT #0 ~Global("#L_SarvQuests","GLOBAL",99) Global("#L_LeavingAfterKorlasz","LOCALS",1)~ THEN BEGIN KORLASZ_EXIT_LEAVE
-		SAY @2284 /* ~It has been an honor.  Farewell.~ */
-		IF ~~ THEN EXIT
-	END
-
-	IF WEIGHT #0 ~Global("#l_SarvQuests","GLOBAL",99) Global("#L_PlayersChoiceAfterKorlasz","LOCALS",1)~ THEN BEGIN KORLASZ_EXIT_ASK
+	IF WEIGHT #-999 ~Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_AjanPK","GLOBAL",2)~ THEN BEGIN POST_KORLASZ_ASK
 		SAY @2278 /* ~I am eager to present our accomplishments before my superiors, but I will stay if you need me.~ */
-		+ ~!Global("#L_LetsHaveFun","GLOBAL",1)~ + @2279 /* ~Please don't go yet.  I still need you, for just a few more days.~ */ DO ~SetGlobal("#L_StayingAfterKorlasz","LOCALS",1)~ EXIT
-		+ ~Global("#L_LetsHaveFun","GLOBAL",1)~ + @2280 /* ~Please let me treat you to a little fun before you go.  Will a day or two make that much difference?~ */ DO ~SetGlobal("#L_StayingAfterKorlasz","LOCALS",1)~ EXIT
-		++ @2281 /* ~Of course you may leave, but not with that armor.~ */ DO ~SetGlobal("#L_StayingAfterKorlasz","LOCALS",1)~ EXIT		
-		++ @2282 /* ~I understand.  You are eager for your knighthood.  Best of luck to you on your journey.~ */ DO ~SetGlobal("#L_LeavingAfterKorlasz","LOCALS",1)~ EXIT
+		+ ~!Global("#L_LetsHaveFun","GLOBAL",1)~ + @2279 /* ~Please don't go yet.  I still need you, for just a few more days.~ */ + POST_KORLASZ_STAY
+		+ ~Global("#L_LetsHaveFun","GLOBAL",1)~ + @2280 /* ~Please let me treat you to a little fun before you go.  Will a day or two make that much difference?~ */  + POST_KORLASZ_STAY
+		++ @2281 /* ~Of course you may leave, but not with that armor.~ */  + POST_KORLASZ_STAY
+		++ @2282 /* ~I understand.  You are eager for your knighthood.  Best of luck to you on your journey.~ */ + POST_KORLASZ_LEAVE
 	END
+
+	IF ~~ THEN BEGIN POST_KORLASZ_STAY
+		SAY @2283 /* ~As you wish.  I owe you that at least.~ */
+		IF ~~ THEN DO ~SetGlobal("#L_AjanPK","GLOBAL",2)~ EXIT
+	END
+
+	IF ~~ THEN BEGIN POST_KORLASZ_LEAVE
+		SAY @2284 /* ~It has been an honor.  Farewell.~ */
+		IF ~~ THEN DO ~SetGlobal("#L_AjanPK","GLOBAL",3) LeaveParty() EscapeArea()~ EXIT
+	END
+
 END
