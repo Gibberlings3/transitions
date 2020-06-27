9a5dc4236979ba93e36437cab80258fadb3e011d
lzenner <lzenner@live.com>
2020-06-27 12:37:01 -1000

    Made sure post-Korlasz exit dialogue wouldn't fire in a dungeon or during combat

--- dialogues/pk_ajantj.d
+++ dialogues/pk_ajantj.d
@@ -8,7 +8,7 @@
 // 		in case he's in SoD or BG2 because he wouldn't carry much with him
 
 APPEND_EARLY AJANTJ
-	IF WEIGHT #-999 ~Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_AjanPK","GLOBAL",2)~ THEN BEGIN POST_KORLASZ_ASK
+	IF WEIGHT #-999 ~Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_AjanPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN BEGIN POST_KORLASZ_ASK
 		SAY @2278 /* ~I am eager to present our accomplishments before my superiors, but I will stay if you need me.~ */
 		+ ~!Global("#L_LetsHaveFun","GLOBAL",1)~ + @2279 /* ~Please don't go yet.  I still need you, for just a few more days.~ */ + POST_KORLASZ_STAY
 		+ ~Global("#L_LetsHaveFun","GLOBAL",1)~ + @2280 /* ~Please let me treat you to a little fun before you go.  Will a day or two make that much difference?~ */  + POST_KORLASZ_STAY
@@ -25,5 +25,4 @@ APPEND_EARLY AJANTJ
 		SAY @2284 /* ~It has been an honor.  Farewell.~ */
 		IF ~~ THEN DO ~SetGlobal("#L_AjanPK","GLOBAL",3) LeaveParty() EscapeArea()~ EXIT
 	END
-
 END
