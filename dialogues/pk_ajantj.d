de43dee2ba8631da5d7757ccdfc300ef051ccaf7
lzenner <lzenner@live.com>
2020-06-27 15:47:22 -1000

    Added post-Korlasz kickout dialogue for Ajantis and tightened trigger evaluation to kick this and normal post-Korlasz dialogue

--- dialogues/pk_ajantj.d
+++ dialogues/pk_ajantj.d
@@ -3,12 +3,11 @@
 // Compiled via main_common.tpa                                        //
 /////////////////////////////////////////////////////////////////////////
 
-// ToDo: Modify kickout dialogue to have him not stay in the area
 // ToDo: Give him a split of the gold but destroy most of it immediately
 // 		in case he's in SoD or BG2 because he wouldn't carry much with him
 
 APPEND_EARLY AJANTJ
-	IF WEIGHT #-999 ~Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_AjanPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN BEGIN POST_KORLASZ_ASK
+	IF WEIGHT #-999 ~Global("#L_StartCaelarAttack","GLOBAL",0) Global("#L_StartBG2","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_AjanPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN BEGIN POST_KORLASZ_ASK
 		SAY @2278 /* ~I am eager to present our accomplishments before my superiors, but I will stay if you need me.~ */
 		+ ~!Global("#L_LetsHaveFun","GLOBAL",1)~ + @2279 /* ~Please don't go yet.  I still need you, for just a few more days.~ */ + POST_KORLASZ_STAY
 		+ ~Global("#L_LetsHaveFun","GLOBAL",1)~ + @2280 /* ~Please let me treat you to a little fun before you go.  Will a day or two make that much difference?~ */  + POST_KORLASZ_STAY
@@ -24,5 +23,13 @@ APPEND_EARLY AJANTJ
 	IF ~~ THEN BEGIN POST_KORLASZ_LEAVE
 		SAY @2284 /* ~It has been an honor.  Farewell.~ */
 		IF ~~ THEN DO ~SetGlobal("#L_AjanPK","GLOBAL",3) LeaveParty() EscapeArea()~ EXIT
+	END
+END
+
+APPEND_EARLY AJANTP
+	IF WEIGHT #-999 ~Global("#L_StartCaelarAttack","GLOBAL",0) Global("#L_StartBG2","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_AjanPK","GLOBAL",3)~ THEN BEGIN POST_KORLASZ_GOODBYE
+		SAY @2316 /* ~I am eager to present our accomplishments before my superiors.  Thank you for all you've done.~ */
+		= @2284 /* ~It has been an honor.  Farewell.~ */
+		IF ~~ THEN DO ~SetGlobal("#L_AjanPK","GLOBAL",3) EscapeArea()~ EXIT
 	END
 END
