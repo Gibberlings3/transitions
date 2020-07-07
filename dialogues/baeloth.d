c5291eb1cee6fd142793135ae902f5201b62f277
lzenner <lzenner@live.com>
2020-07-07 13:26:06 -1000

    Added Baeloth's pre-BG2 exit dialogue

--- dialogues/baeloth.d
+++ dialogues/baeloth.d
@@ -6,7 +6,7 @@
 // ToDo: Give him a split of the gold 
 
 CHAIN
-	IF WEIGHT #-999 ~Global("#L_BaelothModded","GLOBAL",0) GlobalLT("EndOfBG1","GLOBAL",1) Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_NPCPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN BAELOTHJ POST_KORLASZ_ASK
+	IF WEIGHT #-999 ~Global("#L_BaelothModded","GLOBAL",0) Global("EndOfBG1","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_NPCPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN BAELOTHJ POST_KORLASZ_ASK
 		@2364 /* ~This bantering about Baldur's Gate is beyond boring! I have plans.  Huge plans!~ */
 		== JAHEIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0)~ @2326 /* ~I too have other places to be.~ */
 		== KHALIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0) IsValidForPartyDialogue("KHALID") Global("#L_KhalidModded","GLOBAL",0)~ @2338 /* ~Yes, *we* should go.~ */ 
@@ -46,7 +46,7 @@ CHAIN
 	IF ~~ THEN DO ~SetGlobal("#L_NPCPK","GLOBAL",3) SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) ActionOverride("Baeloth",LeaveParty()) ActionOverride("Baeloth",EscapeArea())~ EXIT
 
 CHAIN
-	IF WEIGHT #-998 ~Global("#L_BaelothModded","GLOBAL",0) Global("#L_NPCSoD","GLOBAL",0) GlobalGT("EndOfBG1","GLOBAL",0)~ THEN BAELOTHJ PRE_SOD_WARNING
+	IF WEIGHT #-998 ~Global("#L_BaelothModded","GLOBAL",0) Global("#L_NPCSoD","GLOBAL",0) Global("EndOfBG1","GLOBAL",1) GlobalGT("#L_StartCaelarAttack","GLOBAL",0)~ THEN BAELOTHJ PRE_SOD_WARNING
 		@2365 /* ~The spot lights can wait no longer, <CHARNAME>. Stardom!  A spectacular stage!  No!  No arguments!  That is where I'm headed now.~ */
 		= @2324 /* ~If I am carrying anything you'd prefer to keep, let's exchange equipment now.~ */
 		== JAHEIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0)~ @2329 /* ~I too will be leaving.~ */
@@ -73,21 +73,50 @@ CHAIN
 	END
 	IF ~~ THEN DO ~SetGlobal("#L_NPCSoDLeft","GLOBAL",1) SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) ActionOverride("Baeloth",LeaveParty()) ActionOverride("Baeloth",EscapeArea())~ EXIT
 
+CHAIN
+	IF WEIGHT #-996 ~Global("#L_BaelothModded","GLOBAL",0) Global("#L_NPCBG2","GLOBAL",0) Global("#L_StartBG2","GLOBAL",1)~ THEN BAELOTHJ PRE_BG2_WARNING
+		@2365 /* ~The spot lights can wait no longer, <CHARNAME>. Stardom!  A spectacular stage!  No!  No arguments!  That is where I'm headed now.~ */
+		= @2324 /* ~If I am carrying anything you'd prefer to keep, let's exchange equipment now.~ */
+		== JAHEIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0)~ @2329 /* ~I too will be leaving.~ */
+		== KHALIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0) IsValidForPartyDialogue("KHALID") Global("#L_KhalidModded","GLOBAL",0)~ @2339 /* ~Yes, *we* really must go.~ */ 
+		== KHALIJ IF ~OR(2) !IsValidForPartyDialogue("JAHEIRA") !Global("#L_JaheriaModded","GLOBAL",0) IsValidForPartyDialogue("KHALID") Global("#L_KhalidModded","GLOBAL",0)~ @2335 /* ~I must go as well~ */
+		== AJANTJ IF ~IsValidForPartyDialogue("AJANTIS") Global("#L_AjantisModded","GLOBAL",0)~ @2329 /* ~I too will be leaving.~ */
+		== DORNJ_ IF ~IsValidForPartyDialogue("DORN") Global("#l_DornModded","GLOBAL",0)~ @2343 /* ~Time to go!~ */
+		== EDWINJ_ IF ~IsValidForPartyDialogue("EDWIN") Global("#L_EdwinModded","GLOBAL",0)~ @2350 /* ~I'm so done.~ */
+		== NEERAJ_ IF ~IsValidForPartyDialogue("NEERA") Global("#L_NeeraModded","GLOBAL",0)~ @2357 /* ~Yes, I really do have to go.~ */
+		== GARRIJ IF ~IsValidForPartyDialogue("GARRICK") Global("#L_GarrickModded","GLOBAL",0)~ @2367 /* ~I can't wait any longer, either.~ */
+	END
+	IF ~~ THEN DO ~SetGlobalTimer("#L_NPCBG2","GLOBAL",ONE_MINUTE)~ EXIT
+
+CHAIN
+	IF WEIGHT #-995 ~Global("#L_BaelothModded","GLOBAL",0) GlobalGT("#L_NPCBG2","GLOBAL",0) GlobalTimerExpired("#L_NPCBG2","GLOBAL") Global("#L_NPCBG2Left","GLOBAL",0)~ THEN BAELOTHJ PRE_BG2_GOODBYE
+		@2362 /* ~Look for my name in lights, I'm bowing out of this dump!~ */
+		== JAHEIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0)~ @2328 /* ~Take care of yourself.~ */ DO ~SetGlobal("L_JaheiraOkInBG1Areas","GLOBAL",1) ActionOverride("JAHEIRA",LeaveParty()) ActionOverride("JAHEIRA",EscapeArea())~		
+		== KHALIJ IF ~IsValidForPartyDialogue("KHALID") Global("#L_KhalidModded","GLOBAL",0)~ @2334 /* ~Goodbye.~ */ DO ~SetGlobal("L_KhalidOkInBG1Areas","GLOBAL",1) ActionOverride("KHALID",LeaveParty()) ActionOverride("KHALID",EscapeArea())~
+		== AJANTJ IF ~IsValidForPartyDialogue("AJANTIS") Global("#L_AjantisModded","GLOBAL",0)~ @2284 /* ~It has been an honor.  Farewell.~ */ DO ~SetGlobal("L_AjantisOkInBG1Areas","GLOBAL",1) ActionOverride("AJANTIS",LeaveParty()) ActionOverride("AJANTIS",EscapeArea())~		
+		== DORNJ_ IF ~IsValidForPartyDialogue("DORN") Global("#l_DornModded","GLOBAL",0)~ @2346 /* ~Until we meet again.~ */ DO ~SetGlobal("L_DornOkInBG1Areas","GLOBAL",1) ActionOverride("DORN",LeaveParty()) ActionOverride("DORN",EscapeArea())~
+		== EDWINJ_ IF ~IsValidForPartyDialogue("EDWIN") Global("#L_EdwinModded","GLOBAL",0)~ @2353 /*~I'm outta here!~ */ DO ~SetGlobal("L_EdwinOkInBG1Areas","GLOBAL",1) ActionOverride("EDWIN",LeaveParty()) ActionOverride("EDWIN",EscapeArea())~
+		== NEERAJ_ IF ~IsValidForPartyDialogue("NEERA") Global("#L_NeeraModded","GLOBAL",0)~ @2356 /* ~See ya!  Good luck and all.~ */ DO ~SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) ActionOverride("NEERA",LeaveParty()) ActionOverride("NEERA",EscapeArea())~
+		== GARRIJ IF ~IsValidForPartyDialogue("GARRICK") Global("#L_GarrickModded","GLOBAL",0)~ @2368 /* ~Farewell, <CHARNAME>.  I shall immortalize your name in song!~ */ DO ~SetGlobal("L_GarrickOkInBG1Areas","GLOBAL",1) ActionOverride("GARRICK",LeaveParty()) ActionOverride("GARRICK",EscapeArea())~
+	END
+	IF ~~ THEN DO ~SetGlobal("#L_NPCBG2Left","GLOBAL",1) SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) ActionOverride("Baeloth",LeaveParty()) ActionOverride("Baeloth",EscapeArea())~ EXIT
+
 
 APPEND_EARLY BAELOTHP
-	IF WEIGHT #-999 ~Global("#L_BaelothModded","GLOBAL",0) GlobalLT("EndOfBG1","GLOBAL",1) Global("#l_SarvQuests","GLOBAL",99) GlobalGT("#L_NPCPK","GLOBAL",0)~ THEN BEGIN POST_KORLASZ_FINALLY
+	IF WEIGHT #-999 ~Global("#L_BaelothModded","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) Global("#L_BaelothPK","GLOBAL",0)~ THEN BEGIN POST_KORLASZ_GOODBYE1
+		// Didn't lead the conversation when Korlasz was handed in
+		// Or wasn't in the group at the time
+		// So hasn't given his 'I'm eager' spiel
+		// Let him say it now
+		SAY @2365 /* ~The spot lights can wait no longer, <CHARNAME>. Stardom!  A spectacular stage!  No!  No arguments!  That is where I'm headed now.~  */
+		= @2362 /* ~Look for my name in lights, I'm bowing out of this dump!~ */
+		IF ~~ THEN DO ~SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
+	END
+
+	IF WEIGHT #-998 ~Global("#L_BaelothModded","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) Global("#L_BaelothPK","GLOBAL",1)~ THEN BEGIN POST_KORLASZ_GOODBYE2
 		// We've already done the 'I'm eager' spiel
+		// He's being let go from group after initially being asked to stay
 		SAY @2362 /* ~Look for my name in lights, I'm bowing out of this dump!~ */
-		IF ~~ THEN DO ~SetGlobal("#L_NPCPK","GLOBAL",3) SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
-	END
-
-	IF WEIGHT #-998 ~Global("#L_BaelothModded","GLOBAL",0) GlobalLT("EndOfBG1","GLOBAL",1) Global("#l_SarvQuests","GLOBAL",99) Global("#L_NPCPK","GLOBAL",0)~ THEN BEGIN POST_KORLASZ_GOODBYE
-		SAY @2362 /* ~Look for my name in lights, I'm bowing out of this dump!~ */
-		IF ~~ THEN DO ~SetGlobal("#L_NPCPK","GLOBAL",3) SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
-	END
-
-	IF WEIGHT #-997 ~Global("#L_BaelothModded","GLOBAL",0) GlobalGT("EndOfBG1","GLOBAL",0) GlobalGT("#L_NPCSoD","GLOBAL",0) Global("#L_NPCSoDLeft","GLOBAL",0)~ THEN BEGIN IN_SOD_TIME_TO_GO
-		SAY @2362 /* ~Look for my name in lights, I'm bowing out of this dump!~ */
-		IF ~~ THEN DO ~SetGlobal("#L_NPCSoDLeft","GLOBAL",1) SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
+		IF ~~ THEN DO ~SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
 	END
 END
