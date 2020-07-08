ce0f2a1ea1f71c200c52685ef1b396f0677d93e9
lzenner <lzenner@live.com>
2020-07-07 14:50:04 -1000

    Added Neera's pre-BG2 exit dialogue

--- dialogues/neera.d
+++ dialogues/neera.d
@@ -7,7 +7,7 @@
 // 		in case she's in SoD or BG2 because she'd spend it
 
 CHAIN
-	IF WEIGHT #-999 ~Global("#L_NeeraModded","GLOBAL",0) GlobalLT("EndOfBG1","GLOBAL",1) Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_NPCPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN NEERAJ_ POST_KORLASZ_ASK
+	IF WEIGHT #-999 ~Global("#L_NeeraModded","GLOBAL",0) Global("EndOfBG1","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) GlobalLT("#L_NPCPK","GLOBAL",2) !AreaType(DUNGEON) CombatCounter(0)~ THEN NEERAJ_ POST_KORLASZ_ASK
 		@2358 /* ~You're in the clear now, right?  Can I go?~ */
 		== JAHEIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0)~ @2326 /* ~I too have other places to be.~ */
 		== KHALIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0) IsValidForPartyDialogue("KHALID") Global("#L_KhalidModded","GLOBAL",0)~ @2338 /* ~Yes, *we* should go.~ */ 
@@ -47,7 +47,7 @@ CHAIN
 	IF ~~ THEN DO ~SetGlobal("#L_NPCPK","GLOBAL",3) SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) ActionOverride("Neera",LeaveParty()) ActionOverride("Neera",EscapeArea())~ EXIT
 
 CHAIN
-	IF WEIGHT #-998 ~Global("#L_NeeraModded","GLOBAL",0) Global("#L_NPCSoD","GLOBAL",0) GlobalGT("EndOfBG1","GLOBAL",0)~ THEN NEERAJ_ PRE_SOD_WARNING
+	IF WEIGHT #-998 ~Global("#L_NeeraModded","GLOBAL",0) Global("#L_NPCSoD","GLOBAL",0) Global("EndOfBG1","GLOBAL",1) GlobalGT("#L_StartCaelarAttack","GLOBAL",0)~ THEN NEERAJ_ PRE_SOD_WARNING
 		@2359 /* ~You're great and all, but I really have to get outta here.~ */
 		= @2324 /* ~If I am carrying anything you'd prefer to keep, let's exchange equipment now.~ */
 		== JAHEIJ IF ~IsValidForPartyDialogue("JAHEIRA") Global("#L_JaheriaModded","GLOBAL",0)~ @2329 /* ~I too will be leaving.~ */
@@ -74,22 +74,45 @@ CHAIN
 	END
 	IF ~~ THEN DO ~SetGlobal("#L_NPCSoDLeft","GLOBAL",1) SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) ActionOverride("Neera",LeaveParty()) ActionOverride("Neera",EscapeArea())~ EXIT
 
+CHAIN
+	IF WEIGHT #-996 ~Global("#L_NeeraModded","GLOBAL",0) Global("#L_NPCBG2","GLOBAL",0) Global("#L_StartBG2","GLOBAL",1)~ THEN NEERAJ_ PRE_BG2_WARNING
+		@2359 /* ~You're great and all, but I really have to get outta here.~ */
+		= @2324 /* ~If I am carrying anything you'd prefer to keep, let's exchange equipment now.~ */
+		== AJANTJ IF ~IsValidForPartyDialogue("AJANTIS") Global("#L_AjantisModded","GLOBAL",0)~ @2329 /* ~I too will be leaving.~ */
+		== DORNJ_ IF ~IsValidForPartyDialogue("DORN") Global("#l_DornModded","GLOBAL",0)~ @2343 /* ~Time to go!~ */
+		== EDWINJ_ IF ~IsValidForPartyDialogue("EDWIN") Global("#L_EdwinModded","GLOBAL",0)~ @2350 /* ~I'm so done.~ */
+		== BAELOTHJ IF ~IsValidForPartyDialogue("BAELOTH") Global("#L_BaelothModded","GLOBAL",0)~ @2363	/* ~I couldn't agree more.~ */
+		== GARRIJ IF ~IsValidForPartyDialogue("GARRICK") Global("#L_GarrickModded","GLOBAL",0)~ @2367 /* ~I can't wait any longer, either.~ */
+	END
+	IF ~~ THEN DO ~SetGlobalTimer("#L_NPCBG2","GLOBAL",ONE_MINUTE)~ EXIT
+
+CHAIN
+	IF WEIGHT #-995 ~Global("#L_NeeraModded","GLOBAL",0) GlobalGT("#L_NPCBG2","GLOBAL",0) GlobalTimerExpired("#L_NPCBG2","GLOBAL") Global("#L_NPCBG2Left","GLOBAL",0)~ THEN NEERAJ_ PRE_BG2_GOODBYE
+		@2356 /* ~See ya!  Good luck and all.~ */
+		== AJANTJ IF ~IsValidForPartyDialogue("AJANTIS") Global("#L_AjantisModded","GLOBAL",0)~ @2284 /* ~It has been an honor.  Farewell.~ */ DO ~SetGlobal("L_AjantisOkInBG1Areas","GLOBAL",1) ActionOverride("AJANTIS",LeaveParty()) ActionOverride("AJANTIS",EscapeArea())~		
+		== DORNJ_ IF ~IsValidForPartyDialogue("DORN") Global("#l_DornModded","GLOBAL",0)~ @2346 /* ~Until we meet again.~ */ DO ~SetGlobal("L_DornOkInBG1Areas","GLOBAL",1) ActionOverride("DORN",LeaveParty()) ActionOverride("DORN",EscapeArea())~
+		== EDWINJ_ IF ~IsValidForPartyDialogue("EDWIN") Global("#L_EdwinModded","GLOBAL",0)~ @2353 /*~I'm outta here!~ */ DO ~SetGlobal("L_EdwinOkInBG1Areas","GLOBAL",1) ActionOverride("EDWIN",LeaveParty()) ActionOverride("EDWIN",EscapeArea())~
+		== BAELOTHJ IF ~IsValidForPartyDialogue("BAELOTH") Global("#L_BaelothModded","GLOBAL",0)~ @2362 /* ~Look for my name in lights, I'm bowing out of this dump!~ */ DO ~SetGlobal("L_BaelothOkInBG1Areas","GLOBAL",1) ActionOverride("BAELOTH",LeaveParty()) ActionOverride("BAELOTH",EscapeArea())~
+		== GARRIJ IF ~IsValidForPartyDialogue("GARRICK") Global("#L_GarrickModded","GLOBAL",0)~ @2368 /* ~Farewell, <CHARNAME>.  I shall immortalize your name in song!~ */ DO ~SetGlobal("L_GarrickOkInBG1Areas","GLOBAL",1) ActionOverride("GARRICK",LeaveParty()) ActionOverride("GARRICK",EscapeArea())~
+	END
+	IF ~~ THEN DO ~SetGlobal("#L_NPCBG2Left","GLOBAL",1) SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) ActionOverride("Neera",LeaveParty()) ActionOverride("Neera",EscapeArea())~ EXIT
+
 
 APPEND_EARLY NEERAP_
-	IF WEIGHT #-999 ~Global("#L_NeeraModded","GLOBAL",0) GlobalLT("EndOfBG1","GLOBAL",1) Global("#l_SarvQuests","GLOBAL",99) GlobalGT("#L_NPCPK","GLOBAL",0)~ THEN BEGIN POST_KORLASZ_FINALLY
-		// We've already done the 'I'm eager' spiel
-		SAY @2356 /* ~See ya!  Good luck and all.~ */
-		IF ~~ THEN DO ~SetGlobal("#L_NPCPK","GLOBAL",3) SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
-	END
-
-	IF WEIGHT #-998 ~Global("#L_NeeraModded","GLOBAL",0) GlobalLT("EndOfBG1","GLOBAL",1) Global("#l_SarvQuests","GLOBAL",99) Global("#L_NPCPK","GLOBAL",0)~ THEN BEGIN POST_KORLASZ_GOODBYE
+	IF WEIGHT #-999 ~Global("#L_NeeraModded","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) Global("#L_NeeraPK","GLOBAL",0)~ THEN BEGIN POST_KORLASZ_GOODBYE1
+		// Didn't lead the conversation when Korlasz was handed in
+		// Or wasn't in the group at the time
+		// So hasn't given his 'I'm eager' spiel
+		// Let him say it now
 		SAY @2359 /* ~You're great and all, but I really have to get outta here.~ */
 		= @2356 /* ~See ya!  Good luck and all.~ */
-		IF ~~ THEN DO ~SetGlobal("#L_NPCPK","GLOBAL",3) SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
+		IF ~~ THEN DO ~SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
 	END
 
-	IF WEIGHT #-997 ~Global("#L_NeeraModded","GLOBAL",0) GlobalGT("EndOfBG1","GLOBAL",0) GlobalGT("#L_NPCSoD","GLOBAL",0) Global("#L_NPCSoDLeft","GLOBAL",0)~ THEN BEGIN IN_SOD_TIME_TO_GO
+	IF WEIGHT #-998 ~Global("#L_NeeraModded","GLOBAL",0) Global("#l_SarvQuests","GLOBAL",99) Global("#L_NeeraPK","GLOBAL",1)~ THEN BEGIN POST_KORLASZ_GOODBYE2
+		// We've already done the 'I'm eager' spiel
+		// He's being let go from group after initially being asked to stay
 		SAY @2356 /* ~See ya!  Good luck and all.~ */
-		IF ~~ THEN DO ~SetGlobal("#L_NPCSoDLeft","GLOBAL",1) SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
+		IF ~~ THEN DO ~SetGlobal("L_NeeraOkInBG1Areas","GLOBAL",1) EscapeArea()~ EXIT
 	END
 END
