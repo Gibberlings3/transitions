491e764a32889807dbb59c964c15ce7439645659
lzenner <lzenner@live.com>
2020-07-23 18:22:15 -1000

    Belt's magic item given to the PC now has his voice attached to it and selecting the 'Begin SoD' options brings the group to the Palace to start SoD

--- dialogues/#LAdvGm1.d
+++ dialogues/#LAdvGm1.d
@@ -5,12 +5,12 @@
 
 BEGIN ~#LAdvGm1~
 	IF ~Global("#L_StartCaelarAttack","GLOBAL",0) Global("#L_StartBG2","GLOBAL",0)~ THEN BEGIN ITEM_O1
-		SAY	@2302 /* ~You hear a voice, remarkably similar to Duke Belt's, echo strangely in your mind~ */
+		SAY	@2302 /* ~You hear a voice, remarkably similar to Duke Belt's, echo strangely in your mind~ [BD69824] */
 		= @2303 /* ~So, <CHARNAME>, what would you like to do?~ */
 		+ ~!Global("#L_LetsHaveFun","GLOBAL",1) !Dead("DEATHK")~ + @2308 /* ~Nothing at this time.~ */ + ITEM_FUN
 		+ ~OR(2) Global("#L_LetsHaveFun","GLOBAL",1) Dead("DEATHK")~ + @2308 /* ~Nothing at this time.~ */ EXIT
-		+ ~Global("#L_AcceptedRoom","GLOBAL",1)~ + @2304 /* ~I'd like to go to my room in the palace.~ */ DO ~SetGlobal("#L_SarvQuests","GLOBAL",100) SetGlobal("#L_StartCaelarAttack","GLOBAL",1) SetGlobal("EndOfBG1","GLOBAL",1)~ EXIT
-		+ ~Global("#L_AcceptedRoom","GLOBAL",0) Global("#L_ImoenInPalace","GLOBAL",1)~ + @2307	/* ~I'd like to visit Imoen.~ */ DO ~SetGlobal("#L_SarvQuests","GLOBAL",100) SetGlobal("#L_StartCaelarAttack","GLOBAL",2) SetGlobal("EndOfBG1","GLOBAL",1)~ EXIT
+		+ ~Global("#L_AcceptedRoom","GLOBAL",1)~ + @2304 /* ~I'd like to go to my room in the palace.~ */ DO ~ClearAllActions() SetGlobal("#L_StartCaelarAttack","GLOBAL",1) StartCutSceneMode() StartCutSceneEx("#L_Cut08",TRUE)~ EXIT
+		+ ~Global("#L_AcceptedRoom","GLOBAL",0) Global("#L_ImoenInPalace","GLOBAL",1)~ + @2307	/* ~I'd like to visit Imoen.~ */ DO ~ClearAllActions() SetGlobal("#L_StartCaelarAttack","GLOBAL",2) StartCutSceneMode() StartCutSceneEx("#L_Cut08",TRUE)~ EXIT
 		+ ~Global("#L_AcceptedRoom","GLOBAL",0) Global("#L_ImoenInPalace","GLOBAL",0) OR(3) PartyHasItem("#LKey01") PartyHasItem("#LKey02") PartyHasItem("#LKey03")~ + @2305 /* ~I'd like to go home.~ */ DO ~SetGlobal("#L_SarvQuests","GLOBAL",100) SetGlobal("#L_StartCaelarAttack","GLOBAL",3) SetGlobal("EndOfBG1","GLOBAL",1)~ EXIT
 		+ ~Global("#L_AcceptedRoom","GLOBAL",0) Global("#L_ImoenInPalace","GLOBAL",0) !PartyHasItem("#LKey01") !PartyHasItem("#LKey02") !PartyHasItem("#LKey03")~ + @2306 /* ~I'd like to go to the Elfsong Tavern.~ */ DO ~SetGlobal("#L_SarvQuests","GLOBAL",100) SetGlobal("#L_StartCaelarAttack","GLOBAL",4) SetGlobal("EndOfBG1","GLOBAL",1)~ EXIT
 		++ @2309 /* ~I like to gather a group and explore a bit, maybe see what's south of Nashkel.~ */ DO ~SetGlobal("#L_SarvQuests","GLOBAL",100)  SetGlobal("EndOfBG1","GLOBAL",2) SetGlobal("#L_StartBG2","GLOBAL",1)~ EXIT
