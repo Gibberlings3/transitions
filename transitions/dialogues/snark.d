46e56feae4e93c6cd9f50ff1690837e069fad7db
lzenner <lzenner@live.com>
2020-08-15 20:39:55 -1000

    Revert "Undo"
    This reverts commit 7b07f8b3d0aa34db65828d554e4cf97eb9d3236b.

--- transitions/dialogues/snark.d
+++ transitions/dialogues/snark.d
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////
 // Snarky additions to SoD dialogue to lighten it up a bit //
-// Compiled via snark_n_wit.tpa using snw_sod.tra          //
+// Compiled via add_snark.tpa using snark.tra              //
 /////////////////////////////////////////////////////////////
 
 // Response to a dumb question from Corwin
@@ -50,3 +50,7 @@ EXTEND_BOTTOM BDENTAR 18
 	IF ~!Global("#L_Snark","GLOBAL",0)~ THEN REPLY @3012 /* ~With all due respect sir, I'm not a babysitter.  Neither is the Fist.  But if you insist upon this folly, her fate is upon your head, not mine.~ */ DO ~AddJournalEntry(256390,QUEST)~ GOTO 44
 END
 
+EXTEND_BOTTOM SUELLE2 18
+	IF ~ReputationLT(Player1,10) !Global("#L_Snark","GLOBAL",0)~ THEN REPLY @3013 /* ~Only partially?~ */ + 19
+	IF ~ReputationGT(Player1,9) !Global("#L_Snark","GLOBAL",0)~ THEN REPLY @3013 /* ~Only partially?~ */ + 20
+END
